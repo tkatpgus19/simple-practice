7f938b4c4a344fbf587de4a2045aabfdf9b19bb7
sehyun <germs1020@gmail.com>
2024-01-22 01:23:08 +0900

    init: 프론트, 백엔드 파일 초기화

---
+++ simple-backend/src/test/java/com/ssafy/simplebackend/SimpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.simplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
